c07e9f90eb8c746549d9ed9f416a81b8993c3252
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2023-10-04 10:05:38 -0300

    logica

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+// function jogar(){
+//
+//   let inNumero = document.getElementById("inNumero");
+//
+//   let numero = inNumero.value;
+//
+//   alert("O numero escolhido foi: " + numero)
+//
+//   // document.getElementById("outSaida").textContent = numero
+//
+// }
+//
+// let btnJogar = document.getElementById("btnJogar");
+//
+// btnJogar.addEventListener("click", jogar)
